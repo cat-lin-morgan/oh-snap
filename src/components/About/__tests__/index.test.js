4ec0c9c422ebc73b8d62c29d84ccf0ce2ca758c4
cat - lin <plummorgan@gmail.com>
2020-12-26 22:34:43 -0800

    added tests for nav component

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-//won't have any leftover memory data that could give us false results
 afterEach(cleanup);
 
 describe('About component', () => {
-    it ('renders', () => {
-        render(<About />)
+    it('renders', () => {
+        render(<About />);
     });
 
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />); //makes the snapshot
 
+        expect(asFragment()).toMatchSnapshot();
+    });
 })
