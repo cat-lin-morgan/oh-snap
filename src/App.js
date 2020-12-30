4577e8b6f25e8ba668c669f3d1702e725c29b18c
cat - lin <plummorgan@gmail.com>
2020-12-30 00:31:35 -0800

    added form and form validation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 // import CoolComponent from './components/CoolComponent'
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
       currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About> 
       </main>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
+    const [formState, setFormState] = useState(
+        { 
+            name: '',
+            email: '',
+            message: ''
+        });
+
+    const { name, email, message } = formState;
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            // console.log(isValid); returns a boolean
+            if (!isValid) {
+                setErrorMessage('Your email sucks, learn how to type!');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) { //strings are arrays
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        // console.log('errorMessage', errorMessage);
+
+        if (!errorMessage) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+        // console.log(e);
+        }
+    }
+    // console.log(formState);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        // console.log(formState);
+    }
+
     return (
         <section>
             <h1>
-                <form id='contact-form'>
-                    //name
+                <form id='contact-form' onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor='name'>Name:</label>
-                        <input type='text' name='name' />
+                        <input type='text' defaultValue={name} onChange={handleChange} name='name' />
                     </div>
-                    //email
                     <div >
                         <label htmlFor='email'>Email:</label>
-                        <input type='email' name='email' />
+                        <input type='email' defaultValue={email} onChange={handleChange} name='email' />
                     </div>
-                    //message
                     <div>
                     <label>Message:</label>
-                    <textarea name='message' rows='5' />
+                    <textarea name='message' defaultValue={message} onChange={handleChange} rows='5' />
                     </div>
                     <button type='submit'>Submit</button>
                 </form>
